89ecb3fe660cd2f4a4b71a0aeaaf2a7c99dc2a23
TheMayankRajput <themayankrajput01@gmail.com>
2023-01-15 19:01:30 +0530

    code merged

--- PrabhatProject/Programmerji/urls.py
+++ PrabhatProject/Programmerji/urls.py
@@ -8,12 +8,6 @@ urlpatterns = [
     path('signin',include('my_app.urls')),
     path('forgot',include('my_app.urls')),
     path('home',include('my_app.urls')),
-    path('signup',include('my_app.urls')),
-    path('login',include('my_app.urls')),
-<<<<<<< HEAD
-    path('test',include('my_app.urls')),
-=======
     path('Languages',include('my_app.urls')),
     path('Levels',include('my_app.urls')),
->>>>>>> fcd19046ca084faa470eaaa2cd30b376ab3a0506
 ]
--- PrabhatProject/my_app/urls.py
+++ PrabhatProject/my_app/urls.py
@@ -7,13 +7,6 @@ urlpatterns = [
     path('signin',views.signin),
     path('forgot',views.forgot),
     path('home',views.my_app),
-    path('signup',views.signup),
-    path('login',views.signin),
-<<<<<<< HEAD
-    path('test',views.test),
-]
-=======
     path('Languages',views.Languages),
     path('Levels',views.Levels)
     ]
->>>>>>> fcd19046ca084faa470eaaa2cd30b376ab3a0506
