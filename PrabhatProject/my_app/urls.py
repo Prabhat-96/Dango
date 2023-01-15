a22ee21583fe561f33a418bda2970d150e44f4b8
Prabhat Jha <prabhatjha940@gmail.com>
2023-01-15 17:18:20 +0530

    changes

--- PrabhatProject/my_app/urls.py
+++ PrabhatProject/my_app/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path('home',views.my_app),
     path('signup',views.signup),
     path('login',views.signin),
-<<<<<<< HEAD
+
     path('test',views.test),
-]
-=======
+
+
     path('Languages',views.Languages),
     path('Levels',views.Levels)
     ]
->>>>>>> fcd19046ca084faa470eaaa2cd30b376ab3a0506
+
